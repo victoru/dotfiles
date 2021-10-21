d9e451733cf90905821de211d360ed6e40e9ebe8
Victor Unegbu <victor@unegbu.com>
2021-10-20 22:32:03 -0500

    add nvim.lua configs

---
+++ nvim/etc/nvim/init.lua
@@ -0,0 +1,166 @@
+if (vim.env.XDG_DATA_HOME == nil) then
+  vim.env.XDG_DATA_HOME = vim.env.HOME .. '/.local/share'
+end
+vim.g.python_host_prog = vim.env.HOME .. '/.pyenv/versions/2.7.18/bin/python'
+--vim.g.python3_host_prog = vim.env.XDG_DATA_HOME .. '/virtualenvs/.nvim-6Xe7ujW9/bin/python'
+
+for opt_name, opt_value in pairs({
+  hidden = true,
+  colorcolumn = "80",
+  showmode = false,
+  signcolumn = "yes:1",
+
+  -- double slash to write full paths, prevent clobbering
+  undodir = vim.env.XDG_DATA_HOME .. "/nvim/undo//",
+  backupdir = vim.env.XDG_DATA_HOME .. "/nvim/backup//,/tmp//",
+  directory = vim.env.XDG_DATA_HOME .. "/nvim/swap//",
+  undofile = true,
+
+  scrolloff = 5,             -- keep at least 5 lines above/below
+  sidescrolloff = 5,         -- keep at least 5 lines left/right
+  pastetoggle = "<F12>",
+  equalalways = true,        -- Equal size splits
+  tabstop = 2,               -- Number of spaces that a <Tab> in the file counts for.
+  shiftwidth = 2,            -- Number of spaces to use for each step of (auto)indent.
+  showcmd = true,            -- Show (partial) command in status line.
+  number = true,             -- Show line numbers.
+  ruler = true,              -- Show the line and column number of the cursor position,
+                             -- separated by a comma.
+
+  showmatch = true,          -- When a bracket is inserted, briefly jump to the matching
+                             -- one. The jump is only done if the match can be seen on the
+                             -- screen. The time to show the match can be set with
+                             -- 'matchtime'.
+
+  expandtab = true,          -- Use the appropriate number of spaces to insert a <Tab>.
+                             -- Spaces are used in indents with the '>' and '<' commands
+                             -- and when 'autoindent' is on. To insert a real tab when
+                             -- 'expandtab' is on, use CTRL-V <Tab>.
+
+  smarttab = true,           -- When on, a <Tab> in front of a line inserts blanks
+                             -- according to 'shiftwidth'. 'tabstop' is used in other
+                             -- places. A <BS> will delete a 'shiftwidth' worth of space
+                             -- at the start of the line.
+
+  hlsearch = true,           -- When there is a previous search pattern, highlight all
+                             -- its matches.
+
+  incsearch = true,          -- While typing a search command, show immediately where the
+                             -- so far typed pattern matches.
+
+  ignorecase = true,         -- Ignore case in search patterns.
+
+  smartcase = true,          -- Override the 'ignorecase' option if the search pattern
+                             -- contains upper case characters.
+
+  backspace = "2",           -- Influences the working of <BS>, <Del>, CTRL-W
+                             -- and CTRL-U in Insert mode. This is a list of items,
+                             -- separated by commas. Each item allows a way to backspace
+                             -- over something.
+}) do
+  vim.o[opt_name] = opt_value
+end
+
+require('plugins')
+
+vim.g.tokyonight_style = "night"
+vim.g.colors_name = "tokyonight"
+
+--Remap space as leader key
+vim.api.nvim_set_keymap('', '<Space>', '[Space]', {})
+vim.api.nvim_set_keymap('', '[Space]', '<Nop>', { noremap = true })
+vim.g.mapleader = '[Space]'
+vim.g.maplocalleader = '[Space]'
+
+-- Visual mode keymappings
+-- <TAB>: indent.
+vim.api.nvim_set_keymap('x', '<TAB>', '>', { noremap = true })
+-- <S-TAB>: unindent.
+vim.api.nvim_set_keymap('x', '<S-TAB>', '<', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '>', '>>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<', '<<', { noremap = true })
+vim.api.nvim_set_keymap('x', '>', '>gv', { noremap = true })
+vim.api.nvim_set_keymap('x', '<', '<gv', { noremap = true })
+
+-- Command-line mode keymappings:"{{{
+vim.api.nvim_set_keymap('c', '<C-a>', '<Home>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-b>', '<Left>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-d>', '<Del>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-e>', '<End>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-f>', '<Right>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-n>', '<Down>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-p>', '<Up>', { noremap = true })
+-- <C-k>, K: delete to end.
+vim.api.nvim_set_keymap('c', '<C-k>', [[<C-\>e getcmdpos() == 1 ? "" : getcmdline()[:getcmdpos()-2]<CR>]], { noremap = true })
+-- <C-y>: paste.
+vim.api.nvim_set_keymap('c', '<C-y>', '<C-r>*', { noremap = true })
+
+-- vim.api.nvim_set_keymap('n', '[Space]ev', ':<C-u>edit $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[Space]rv', ':<C-u>source $MYVIMRC | echo "source $MYVIMRC"<CR>', { noremap = true, silent = true })
+
+-- Useful save mappings?
+vim.api.nvim_set_keymap('n', '<Leader><Leader>', ':<C-u>update<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>v', 'V`]', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-c>', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-c>', '<ESC>', { noremap = true })
+
+-- Up Down Left Right resize splits
+vim.api.nvim_set_keymap('n', '<up>', '<c-w>+', { noremap = true })
+vim.api.nvim_set_keymap('n', '<down>', '<c-w>-', { noremap = true })
+vim.api.nvim_set_keymap('n', '<left>', '<c-w><', { noremap = true })
+vim.api.nvim_set_keymap('n', '<right>', '<c-w>>', { noremap = true })
+
+
+-- H and L keys for buffer movement
+vim.api.nvim_set_keymap('', 'H', ':bp<CR>', { noremap = true })
+vim.api.nvim_set_keymap('', 'L', ':bn<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('', 'j', 'gj', { noremap = true })
+vim.api.nvim_set_keymap('', 'k', 'gk', { noremap = true })
+
+-- remove trailing whitespaces
+vim.api.nvim_set_keymap('n', '<Leader>W', [[:let _s=@/ <Bar> :%s/\s\+$//e <Bar> :let @/=_s <Bar> :nohl <Bar> :unlet _s <CR>]], { noremap = true })
+
+-- git gutter?
+vim.cmd('highlight! link SignColumn LineNr')
+vim.cmd([[
+augroup reload_vimrc
+autocmd!
+    autocmd bufwritepost $MYVIMRC nested source $MYVIMRC
+augroup END
+augroup Crosshair
+    au!
+    autocmd WinLeave *  setlocal nocursorcolumn nocursorline
+    autocmd VimEnter,BufWinEnter,WinEnter * setlocal cursorline cursorcolumn
+    " invert current line colors
+    au ColorScheme,VimEnter,BufWinEnter,WinEnter * hi CursorLine cterm=reverse
+    au ColorScheme,VimEnter,BufWinEnter,WinEnter * hi CursorColumn cterm=reverse
+augroup END
+
+autocmd VimEnter * silent exec "!kill -s SIGWINCH" getpid()
+autocmd FileType TelescopePrompt lua require('cmp').setup.buffer { enabled = false }
+]])
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+--vim.cmd([[
+  --vim.o.updatetime = 250
+  --vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
+--]])
+--
+
+--Highlight line number instead of having icons in sign column
+--See the properties of the signs with sign list.
+vim.cmd [[
+  highlight LspDiagnosticsLineNrError guibg=#51202A guifg=#FF0000 gui=bold
+  highlight LspDiagnosticsLineNrWarning guibg=#51412A guifg=#FFA500 gui=bold
+  highlight LspDiagnosticsLineNrInformation guibg=#1E535D guifg=#00FFFF gui=bold
+  highlight LspDiagnosticsLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
+
+  sign define DiagnosticSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsLineNrError
+  sign define DiagnosticSignWarn text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsLineNrWarning
+  sign define DiagnosticSignInfo text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsLineNrInformation
+  sign define DiagnosticSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsLineNrHint
+]]
---
+++ nvim/etc/nvim/lua/plugins.lua
@@ -0,0 +1,431 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile profile=true
+  augroup end
+]])
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-vinegar'
+  use 'tpope/vim-fugitive'
+  use 'scrooloose/nerdcommenter'
+  use 'folke/tokyonight.nvim'
+  use 'dracula/vim'
+  use 'bluz71/vim-moonfly-colors'
+  use 'Pocco81/Catppuccino.nvim'
+  use 'sjl/gundo.vim'
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  use {
+    'sunjon/Shade.nvim',
+    config = function()
+      require'shade'.setup({
+        overlay_opacity = 35,
+        opacity_step = 1,
+        keys = {
+          --brightness_up    = '<C-Up>',
+          --brightness_down  = '<C-Down>',
+          toggle           = '<Leader>s',
+        }
+      })
+    end
+  }
+
+  use {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup{}
+    end
+  }
+
+  use {
+    'vim-scripts/kwbdi.vim',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<C-W>!', '<Plug>Kwbd', {})
+    end
+  }
+
+  use {
+    "folke/trouble.nvim",
+    requires = {'kyazdani42/nvim-web-devicons'},
+    config = function()
+      require("trouble").setup {}
+      vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+        {silent = true, noremap = true}
+      )
+      vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+        {silent = true, noremap = true}
+      )
+    end
+  }
+
+  -- Post-install/update hook with call of vimscript function with argument
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        -- ensure_installed = "all",
+        highlight = {
+          enable = true
+        },
+      }
+    end
+  }
+
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      {'williamboman/nvim-lsp-installer'},
+      { 'hrsh7th/cmp-nvim-lsp' }, -- LSP source for nvim-cmp
+    },
+    config = function()
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+        -- Enable completion triggered by <c-x><c-o>
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        local opts = { noremap=true, silent=true }
+
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', '[Space]wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '[Space]wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        buf_set_keymap('n', '[Space]wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+        buf_set_keymap('n', '[Space]D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        buf_set_keymap('n', '[Space]rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        buf_set_keymap('n', '[Space]ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+        buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+        buf_set_keymap('n', '[Space]e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+        buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+        buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+        buf_set_keymap('n', '[Space]q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts) --
+        buf_set_keymap('n', '[Space]f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+        --
+        -- Set some keybinds conditional on server capabilities
+        if client.resolved_capabilities.document_formatting then
+          buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        elseif client.resolved_capabilities.document_range_formatting then
+          buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        end
+
+        -- Set autocommands conditional on server_capabilities
+        if client.resolved_capabilities.document_highlight then
+          vim.api.nvim_exec([[
+            hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+            hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+            hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+            augroup lsp_document_highlight
+              autocmd! * <buffer>
+              autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+              autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END
+          ]], false)
+        end
+
+      end
+
+      -- Add additional capabilities supported by nvim-cmp
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      local lsp_installer = require("nvim-lsp-installer")
+      lsp_installer.on_server_ready(function(server)
+        local opts = {
+          on_attach = on_attach,
+          capabilities = capabilities,
+           flags = {
+             debounce_text_changes = 150,
+           }
+        }
+
+        if server.name == "diagnosticls" then
+            opts.filetypes = {"sh", "json", "yaml", "yaml.cloudformation"}
+            opts.init_options = {
+               filetypes = {
+                 sh = "shellcheck",
+                 ["yaml.cloudformation"] = "cfnlint",
+               },
+               linters = {
+                 cfnlint = {
+                   command = "cfn-lint",
+                   args = {"--format", "json", "-"},
+                   sourceName = "cfnlint",
+                   parseJson = {
+                     line = "Location.Start.LineNumber",
+                     column = "Location.Start.ColumnNumber",
+                     endLine = "Location.End.LineNumber",
+                     endColumn = "Location.End.ColumnNumber",
+                     message = "${Message} [${Rule.Id}]",
+                     security = "Level"
+                   },
+                   securities = {
+                     error = "Error",
+                     warning = "Warning",
+                     info = "Informational",
+                     unknown = "Unknown",
+                     -- style = "Hint"
+                   }
+                 },
+                 shellcheck = {
+                   command = "shellcheck",
+                   debounce = 100,
+                   args = {"--format", "json", "-"},
+                   sourceName = "shellcheck",
+                   parseJson = {
+                     line = "line",
+                     column = "column",
+                     endLine = "endLine",
+                     endColumn = "endColumn",
+                     message = "${message} [${code}]",
+                     security = "level"
+                   },
+                   securities = {
+                     error = "error",
+                     warning = "warning",
+                     info = "info",
+                     style = "hint"
+                   }
+                 },
+               }
+            }
+         elseif server.name == 'yamlls' then
+           opts.settings = {
+             format = {
+               enable = true,
+             },
+             hover = true,
+             completion = true,
+             yaml = {
+               customTags = {
+                 "!And sequence",
+                 "!Base64 mapping",
+                 "!Cidr scalar",
+                 "!Cidr sequence",
+                 "!Condition scalar",
+                 "!Condition",
+                 "!Equals sequence",
+                 "!FindInMap sequence",
+                 "!Fn",
+                 "!GetAZs scalar",
+                 "!GetAZs",
+                 "!GetAtt scalar",
+                 "!GetAtt sequence",
+                 "!GetAtt",
+                 "!If sequence",
+                 "!If",
+                 "!ImportValue mapping",
+                 "!ImportValue scalar",
+                 "!ImportValue",
+                 "!Join sequence",
+                 "!Not sequence",
+                 "!Or sequence",
+                 "!Ref Scalar",
+                 "!Ref scalar",
+                 "!Ref",
+                 "!Select",
+                 "!Split sequence",
+                 "!Split",
+                 "!Sub scalar",
+                 "!Sub sequence",
+                 "!Sub",
+                 "!Transform mapping",
+               }
+             }
+           }
+        end
+        -- This setup() function is exactly the same as lspconfig's setup function
+        -- (:help lspconfig-quickstart)
+        server:setup(opts)
+        vim.cmd [[ do User LspAttachBuffers ]]
+      end)
+
+      -- Enable diagnostics
+      vim.g.diagnostics_visible = true
+      vim.lsp.handlers["textDocument/publishDiagnostics"] =
+        vim.lsp.with(
+          vim.lsp.diagnostic.on_publish_diagnostics,
+          {
+            underline = true,
+            virtual_text = true,
+            signs = true,
+            update_in_insert = true
+          }
+        )
+    end,
+  }
+
+  use {
+    'hrsh7th/nvim-cmp', -- Autocompletion plugin
+    requires = {
+      { 'L3MON4D3/LuaSnip' }, -- Snippets plugin
+      { 'hrsh7th/cmp-nvim-lsp' }, -- LSP source for nvim-cmp
+      { 'hrsh7th/cmp-buffer' }, -- Buffer source for nvim-cmp
+      { 'saadparwaiz1/cmp_luasnip' }, -- Snippets source for nvim-cmp
+    },
+    config = function()
+      -- Set completeopt to have a better completion experience
+      vim.o.completeopt = 'menuone,noselect'
+      local luasnip = require 'luasnip'
+      local cmp = require 'cmp'
+      cmp.setup {
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end,
+          ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+        },
+      }
+    end
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} },
+    config = function()
+      vim.cmd([[
+        nnoremap <leader>ff <cmd>Telescope find_files<cr>
+        nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+        nnoremap <leader>fb <cmd>Telescope buffers<cr>
+        nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+      ]])
+
+    end
+  }
+  use {
+    'shadmansaleh/lualine.nvim',
+    requires = {
+      {"folke/twilight.nvim"},
+      {'kyazdani42/nvim-web-devicons'},
+    },
+    config = function()
+      require'lualine'.setup {
+        options = {theme = 'tokyonight'},
+        extensions = {'fugitive','nvim-tree'}
+      }
+    end
+  }
+
+  use {
+    "ray-x/go.nvim",
+    config = function()
+      require("go").setup()
+      --require("go.format").gofmt()  -- format only
+      --require("go.format").goimport()  -- goimport + gofmt
+      ---- Format on save
+      vim.api.nvim_exec([[
+        autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
+      ]], false)
+      -- Import on save
+      vim.api.nvim_exec([[
+        autocmd BufWritePre *.go :silent! lua require('go.format').goimport()
+      ]], false)
+
+      vim.cmd([[
+          autocmd BufWritePre (InsertLeave?) <buffer> lua vim.lsp.buf.formatting_sync(nil,500)
+      ]])
+    end,
+    requires = { {'nvim-treesitter/nvim-treesitter-textobjects'} },
+  }
+
+  use {
+    "nvim-neorg/neorg",
+    config = function()
+      require('neorg').setup {
+        -- Tell Neorg what modules to load
+        load = {
+          ["core.defaults"] = {}, -- Load all the default modules
+          ["core.norg.concealer"] = {}, -- Allows for use of icons
+          ["core.norg.dirman"] = { -- Manage your directories with Neorg
+            config = {
+              workspaces = {
+                my_workspace = "~/neorg"
+              }
+            }
+          }
+        },
+      }
+    end,
+    requires = "nvim-lua/plenary.nvim"
+  }
+end)
+-- gopls eslint vuels vimls bashls pyright jsonls tsserver yamlls html dockerls diagnosticls
+
